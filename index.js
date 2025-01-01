e964c99e95b68b0ef20b4ca436a21ed5faa6a7ca
Kushal Harsora <kushalharsora13@gmail.com>
2025-01-01 16:22:13 +0530

    backend deploy

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // import express
 const express = require("express");
 const cors = require("cors");
+
 // create the app
 const app = express();
-// define port number
-const port = 5000;
 
-app.use(cors())
+// define port number
+const port = process.env.PORT || 5000;
+
+// add middleware
+app.use(cors({
+    origin: ["http://localhost:3000/", "https://restaurant-book.vercel.app/"]
+}))
 
 // create a get request
 app.get('/', (request, response) => {
@@ -18,3 +23,5 @@ app.get('/', (request, response) => {
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
 });
+
+module.exports =  app;
