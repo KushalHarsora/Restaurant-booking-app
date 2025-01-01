e5e8edc36ebf2173a5f87aab57613867379b0db3
Kushal Harsora <kushalharsora13@gmail.com>
2025-01-01 16:41:57 +0530

    backend connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 'use client';
 
-// import { useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 
-export default function Home() {
+const Home = () => {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse = await axios.get(`${process.env.NEXT_PUBLIC_domain!}/`);
+        const { message } = response.data;
+        alert(message);
+      } catch (error) {
+        alert(`error: ${error}`);
+      }
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <main className=" h-screen w-screen flex justify-center items-center">
@@ -10,3 +25,5 @@ export default function Home() {
     </main>
   );
 }
+
+export default Home
