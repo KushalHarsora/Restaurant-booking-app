d31b9a99d951d674d9a3f1de2229bc29789a0156
Kushal Harsora <kushalharsora13@gmail.com>
2025-01-01 15:49:21 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+// import { useEffect } from "react";
+
+export default function Home() {
+
+  return (
+    <main className=" h-screen w-screen flex justify-center items-center">
+      Hello World
+    </main>
+  );
+}
